7b06159fd3cb986eb8cf7af79a803de99f582072
Роман Рошин <r.roshin.dodo@gmail.com>
2026-07-13 21:49:52 +0400

    Synchronize extension and page bridge version

--- browser_injector/content.js
+++ browser_injector/content.js
@@ -3,7 +3,7 @@
     return;
   }
 
-  const bridgeVersion = "2026-07-13-quest-scope-v50";
+  const bridgeVersion = "2026-07-13-quest-scope-v51";
   const contentSource = `antibot-cv-content:${bridgeVersion}`;
   const injectorSource = `antibot-cv-injector:${bridgeVersion}`;
   let clientId = "";
